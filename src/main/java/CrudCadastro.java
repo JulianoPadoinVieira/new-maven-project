016f5a1c14552689e89f4f7f9adeab9bb2fdee4a
Juliano <kurdpemgs@gmail.com>
2026-06-02 23:39:42 -0300

    CRUD List

--- src/main/java/CrudCadastro.java
+++ src/main/java/CrudCadastro.java
@@ -1,5 +1,10 @@
+import com.mysql.cj.MysqlConnection;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CrudCadastro {
@@ -19,8 +24,8 @@ public class CrudCadastro {
             con.setInt(2, cadastro.getAge());
             con.setString(3, cadastro.getEmail());
 
-            int rows = con.executeUpdate();
-            System.out.println(rows + " cliente inserido com sucesso.");
+            con.executeUpdate();
+            System.out.println("Cliente inserido com sucesso.");
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -28,30 +33,30 @@ public class CrudCadastro {
 
     public void alterar(CadastroCliente cadastro){
 
-        String sqlInstruction = "UPDATE cadastro SET name = ? email = ? WHERE id = ?";
-        int id = 0;
+        String sqlInstruction = "UPDATE cadastro SET nome = ?, email = ? WHERE id = ?";
 
         try (PreparedStatement con = connection.prepareStatement(sqlInstruction)) {
             con.setString(1, cadastro.getEmail());
-            con.setInt(2, 4);
+            con.setString(2, cadastro.getEmail());
+            con.setInt(3, cadastro.getId());
 
-            int rows = con.executeUpdate();
-            System.out.println(rows + " informação de cliente atualizada com sucesso. ");
+            con.executeUpdate();
+            System.out.println("Informações do cliente com ID; " + cadastro.getId() + " atualizadas com sucesso.");
         } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
 
-    public void excluir(CadastroCliente cadastro){
+    public void excluir(int id){
 
         String sqlInstruction = "DELETE FROM cadastro WHERE id = ?";
 
         try (PreparedStatement con = connection.prepareStatement(sqlInstruction)) {
-            con.setInt(2, cadastro.getId());
+            con.setInt(1, id);
 
-            int rows = con.executeUpdate();
-            System.out.println("O usuário " + 5 + " foi deletado com sucesso.");
+            con.executeUpdate();
+            System.out.println("O usuário com ID número " + id + " foi deletado com sucesso.");
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -59,10 +64,60 @@ public class CrudCadastro {
     }
 
     public List<CadastroCliente> listar(){
-        return null;
+
+        List<CadastroCliente> lista = new ArrayList<>();
+
+        try {
+            String sqlInstruction = "SELECT id, nome, email FROM cadastro;";
+            PreparedStatement statement = connection.prepareStatement(sqlInstruction);
+            ResultSet result = statement.executeQuery();
+
+            while (result.next()) {
+                int id = result.getInt("id");
+                String nome = result.getString("nome");
+                int idade = result.getInt("idade");
+                String email = result.getString("email");
+                System.out.println(id + " - " + nome + " - " + idade + " - " + email);
+
+                CadastroCliente cadastro = new CadastroCliente();
+                cadastro.setId(id);
+                cadastro.setName(nome);
+                cadastro.setAge(idade);
+                cadastro.setEmail(email);
+
+                lista.add(cadastro);
+
+
+
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return lista;
     }
 
     public CadastroCliente buscar(){
+
+//        String sqlInstruction = "SELECT * FROM cadastro";
+//
+//        try (PreparedStatement con = connection.prepareStatement(sqlInstruction)
+//            Statement stmt = con.createStatement();
+//            ResultSet rs = stmt.executeQuery(sqlInstruction)){
+//            while (rs.next()) {
+//                System.out.println(rs.getInt("ID: ") + " | " +
+//                        rs.getString("Nome: ") + " | " +
+//                        rs.getInt("Idade: ") + " | " +
+//                        rs.getString("E-mail: "));
+//            }
+//
+//
+//        }catch(Exception e) {
+//            e.printStackTrace();
+//        }
+
         return null;
     }
 
--- src/main/java/SistemaCadastro.java
+++ src/main/java/SistemaCadastro.java
@@ -1,22 +1,27 @@
 public class SistemaCadastro {
     public static void main(String[] args) {
 
-        CadastroCliente cadastroCliente = new CadastroCliente();
+
         CrudCadastro crudCadastro = new CrudCadastro();
 
+        //Salvar
+        CadastroCliente cadastroCliente = new CadastroCliente();
         cadastroCliente.setName("Chica");
         cadastroCliente.setAge(19);
         cadastroCliente.setEmail("chica@hotmail.com");
-
         crudCadastro.salvar(cadastroCliente);
 
+        //Atualizar
         CadastroCliente atualizarCliente = new CadastroCliente();
-        atualizarCliente.setName("Dorothy");
-        atualizarCliente.setEmail("dorothy@gmail.com");
+        atualizarCliente.setName("Ezequiel");
+        atualizarCliente.setEmail("ezequiel@gmail.com");
+        atualizarCliente.setId(5);
         crudCadastro.alterar(atualizarCliente);
 
         CadastroCliente deletarCliente = new CadastroCliente();
-        deletarCliente.setId(5);
+//        deletarCliente.setId(18);
+        crudCadastro.excluir(26);
+
 
 
 
